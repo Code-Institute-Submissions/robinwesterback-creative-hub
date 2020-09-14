db9a48149eb13d1a544ebd0e227e9b22bad18d58
Robin Westerback <robinwesterback1989@gmail.com>
2020-09-14 16:49:56 +0000

    Add templates folder and estabslish connection to MongoDB

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'creativeHub'
+app.config["MONGO_URI"] = 'mongodb+srv://robin:Kungen1989@myfirstcluster.lxmdn.mongodb.net/creativeHub?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
+
 
 @app.route('/')
-def test():
-    return 'Test'
+@app.route('/get_work')
+def get_work():
+    return render_template("work.html", work=mongo.db.work.find())
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-        port=int(os.environ.get('PORT')),
-        debug=True)
+            port=int(os.environ.get('PORT')),
+            debug=True)
