f8b8c29cebb16ffc58667b6542baddd30a4f77d2
Robin Westerback <robinwesterback1989@gmail.com>
2020-09-23 17:28:14 +0000

    Add input fields and a submit button to create a creative account.

--- app.py
+++ app.py
@@ -16,9 +16,18 @@ mongo = PyMongo(app)
 def get_creatives():
     return render_template("creatives.html", creatives=mongo.db.creatives.find())
 
+
 @app.route('/create_creative')
 def create_creative():
     return render_template('createCreative.html')
+
+
+@app.route('/insert_creative', methods=['POST'])
+def insert_creative():
+    creatives = mongo.db.creatives
+    creatives.insert_one(request.form.to_dict())
+    return redirect(url_for('get_creatives'))
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
