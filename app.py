ecf9388ace74cb80f346fe45b1cc1696009ea982
Robin Westerback <robinwesterback1989@gmail.com>
2020-09-28 16:14:47 +0000

    Add content to the templates and update the register function

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def home():
 
 @app.route('/user_interface')
 def user_interface():
-    if 'username' in session:
+    if 'email' in session:
         return render_template('userInterface.html', page_title='User Interface')
 
     return render_template('login.html', page_title='Login')
@@ -34,29 +34,37 @@ def user_interface():
 @app.route('/login', methods=['POST'])
 def login():
     users = mongo.db.users
-    login_user = users.find_one({'name' : request.form['username']})
+    login_user = users.find_one({'email': request.form['email']})
 
     if login_user:
-        if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password']) == login_user['password']:
-            session['username'] = request.form['username']
+        if bcrypt.hashpw(request.form['password'].encode('utf-8'), login_user['password']) == login_user['password']:
+            session['email'] = request.form['email']
             return redirect(url_for('user_interface'))
 
-    return 'Invalid username/password combination'
+    return 'Invalid email/password combination'
 
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
     if request.method == 'POST':
         users = mongo.db.users
-        existing_user = users.find_one({'name' : request.form['username']})
+        existing_user = users.find_one({'email': request.form['email']})
 
         if existing_user is None:
-            hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
-            users.insert({'name' : request.form['username'], 'password' : hashpass})
-            session['username'] = request.form['username']
-            return redirect(url_for('login'))
+            hashpass = bcrypt.hashpw(
+                request.form['password'].encode('utf-8'), bcrypt.gensalt())
+            users.insert({'email': request.form['email'],
+                          'first_name': request.form.get('first_name'),
+                          'last_name': request.form.get('last_name'),
+                          'email': request.form.get('email'),
+                          'phone': request.form.get('phone'),
+                          'city': request.form.get('city'),
+                          'country': request.form.get('country'),
+                          'password': hashpass})
+            session['email'] = request.form['email']
+            return redirect(url_for('user_interface'))
 
-        return 'That username already exists!'
+        return 'That email already exists!'
 
     return render_template('register.html')
 
@@ -79,7 +87,7 @@ def insert_creative():
     return redirect(url_for('get_creatives'))
 
 
-@app.route('/edit_creative/<creative_id>')
+@app.route('/edit_user/<creative_id>')
 def edit_creative(creative_id):
     the_creative = mongo.db.creatives.find_one({"_id": ObjectId(creative_id)})
     return render_template('editCreative.html', creative=the_creative)
@@ -89,7 +97,7 @@ def edit_creative(creative_id):
 def update_creative(creative_id):
     creatives = mongo.db.creatives
     creatives.update({'_id': ObjectId(creative_id)},
-    {
+                     {
         'first_name': request.form.get('first_name'),
         'last_name': request.form.get('last_name'),
         'email': request.form.get('email'),
