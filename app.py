8889f4c14eb103ebb6b802e2aa8940a8350dd90d
Robin Westerback <robinwesterback1989@gmail.com>
2021-01-20 17:20:19 +0000

    Change secret key

--- app.py
+++ app.py
@@ -709,7 +709,7 @@ def delete_brief(brief_id):
 
 
 if __name__ == '__main__':
-    app.secret_key = 'mysecret'
+    app.secret_key = os.environ.get("SECRET_KEY")
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
             debug=False)
